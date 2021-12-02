6869b224f6bfadbd5ffb7c80a89138547ba4d2d3
kirtisonje <92393855+kirtisonje@users.noreply.github.com>
2021-12-02 22:02:50 +0530

    Add files via upload

---
+++ shape.js
@@ -0,0 +1,19 @@
+// declare class
+
+// export class using module.exports
+class Shape {
+   
+    constructor(color) {
+        this.color = "RED";
+    }
+  
+    drawShape() {
+        console.log(`${this.color} is colored`);
+  
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is to be calculated`)
+    }
+  
+  }
+  module.exports = Shape
